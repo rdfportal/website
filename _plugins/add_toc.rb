8e4e64fd44b7ed3b52fb3264d001d566c8bf6b24
永野 朗夫 <nagano@me.com>
2026-03-23 17:47:14 +0900

    toc styling

--- _plugins/add_toc.rb
+++ _plugins/add_toc.rb
@@ -6,7 +6,7 @@ Jekyll::Hooks.register [:pages, :documents], :pre_render do |doc, payload|
   if doc.data['toc'] && (doc.extname == '.md' || doc.extname == '.markdown')
     unless doc.content.include?('{:toc')
       # The Kramdown syntax * TOC {:toc} will automatically generate the table of contents list.
-      doc.content = "<div class=\"toc-wrapper\" markdown=\"1\">\n<h3 class=\"toc-title\">Table of Contents</h3>\n\n* TOC\n{:toc .page-toc}\n\n</div>\n\n" + doc.content
+      doc.content = "<div class=\"toc-view\" markdown=\"1\">\n\n* TOC\n{:toc .page}\n\n</div>\n\n" + doc.content
     end
   end
 end
